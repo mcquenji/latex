d37991b66ca885be886f62bc3617fee9feccb8b4
bmceachnie <60017181+McQuenji@users.noreply.github.com>
2024-04-23 18:45:33 +0200

    Update generate_comment.py and pr_preview.yaml workflows

--- .github/workflows/generate_comment.py
+++ .github/workflows/generate_comment.py
@@ -1,9 +1,12 @@
 import os
+import sys
 
 
 def main():
     errors = []
     warnings = []
+
+    ARTIFACT_NAME = sys.argv[1]
 
     with open("main.log", "r") as file:
         for line in file:
@@ -32,7 +35,11 @@ def main():
         comment_body += "No critical issues found.\n"
 
     if os.path.exists("main.pdf"):
-        comment_body += "[Download the compiled PDF](https://github.com/${{ github.repository }}/actions/artifacts/${{ github.run_id }}?artifactName=pr-preview)\n"
+        comment_body += (
+            "[Download the compiled PDF](https://github.com/${{ github.repository }}/actions/artifacts/${{ github.run_id }}?artifactName="
+            + f"{ARTIFACT_NAME}"
+            + ")\n"
+        )
 
     with open("comment_body.md", "w") as file:
         file.write(comment_body)
