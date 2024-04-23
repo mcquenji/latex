7ec30b3fadddc8adbb51eb5ff7fd4b9ee58b8f95
bmceachnie <60017181+McQuenji@users.noreply.github.com>
2024-04-23 22:08:39 +0200

    added whitelist

--- .github/workflows/check_logs.py
+++ .github/workflows/check_logs.py
@@ -1,17 +1,35 @@
 import os
 import sys
+import re
+
+
+def load_whitelist() -> list[str]:
+    with open(".github/workflows/ignore.conf", "r") as file:
+        return [line.strip() for line in file if not line.startswith("#")]
+
+
+def is_whitelisted(line: str, whitelist: list[str]) -> bool:
+    for pattern in whitelist:
+        if re.search(pattern, line):
+            return True
+    return False
 
 
 def main():
+    WHITELIST = load_whitelist()
+
     fail_on_warning = os.getenv("FAIL_ON_WARNING", "false").lower() == "true"
     with open("main.log", "r") as file:
         for line in file:
+            found_error = False
+
             if "error: " in line.lower():
-                sys.exit(1)  # Exit with error if there are errors
+                found_error = True
             if "warning: " in line.lower() and fail_on_warning:
-                sys.exit(
-                    1
-                )  # Exit with error if FAIL_ON_WARNING is true and there are warnings
+                found_error = True
+
+            if found_error and not is_whitelisted(line, WHITELIST):
+                sys.exit(1)  # Exit with error if a critical issue is found
 
     sys.exit(0)  # Exit successfully if no critical issues
 
--- .github/workflows/generate_comment.py
+++ .github/workflows/generate_comment.py
@@ -1,19 +1,47 @@
 import os
 import sys
+import re
+
+
+def load_whitelist() -> list[str]:
+    with open(".github/workflows/ignore.conf", "r") as file:
+        return [line.strip() for line in file if not line.startswith("#")]
+
+
+def is_whitelisted(line: str, whitelist: list[str]) -> bool:
+    for pattern in whitelist:
+        if re.search(pattern, line):
+            return True
+    return False
 
 
 def main():
+    WHITELIST = load_whitelist()
+
     errors = []
     warnings = []
+    whitelisted = []
 
     DOWNLOAD_URL = sys.argv[1]
 
     with open("main.log", "r") as file:
         for line in file:
+            found_error = False
+            is_warning = False
+
             if "error: " in line.lower():
-                errors.append(line.strip())
-            if "warning: " in line.lower():
-                warnings.append(line.strip())
+                found_error = True
+            elif "warning: " in line.lower():
+                found_error = True
+                is_warning = True
+
+            if found_error:
+                if is_whitelisted(line, WHITELIST):
+                    whitelisted.append(line)
+                elif is_warning:
+                    warnings.append(line)
+                else:
+                    errors.append(line)
 
     comment_body = "Build results:\n"
     if errors or warnings:
@@ -31,6 +59,13 @@ def main():
             )
     else:
         comment_body += "No critical issues found.\n"
+
+    if whitelisted:
+        comment_body += (
+            "<details><summary>Suppressed errors & warnings</summary>\n\n```\n"
+            + "\n".join(whitelisted)
+            + "\n```\n</details>\n"
+        )
 
     if os.path.exists("main.pdf"):
         comment_body += f"[Download the compiled PDF]({DOWNLOAD_URL})\n"
