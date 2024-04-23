4390903b2628ea20cee880cabb52405f9f05df50
bmceachnie <60017181+McQuenji@users.noreply.github.com>
2024-04-23 21:19:25 +0200

    fix pdf download link and error/warning detection

--- .github/workflows/generate_comment.py
+++ .github/workflows/generate_comment.py
@@ -6,13 +6,15 @@ def main():
     errors = []
     warnings = []
 
-    ARTIFACT_NAME = sys.argv[1]
+    REPO = sys.argv[1]
+    RUN_ID = sys.argv[2]
+    ARTIFACT_NAME = sys.argv[3]
 
     with open("main.log", "r") as file:
         for line in file:
-            if "error" in line.lower():
+            if "error: " in line.lower():
                 errors.append(line.strip())
-            if "warning" in line.lower():
+            if "warning: " in line.lower():
                 warnings.append(line.strip())
 
     comment_body = "Build results:\n"
@@ -27,17 +29,13 @@ def main():
             comment_body += (
                 "<details><summary>Warnings</summary>\n\n```\n"
                 + "\n".join(warnings)
-                + "\n```\n</details>\n"
+                + "\n```\n</details>\n\n"
             )
     else:
         comment_body += "No critical issues found.\n"
 
     if os.path.exists("main.pdf"):
-        comment_body += (
-            "[Download the compiled PDF](https://github.com/${{ github.repository }}/actions/artifacts/${{ github.run_id }}?artifactName="
-            + f"{ARTIFACT_NAME}"
-            + ")\n"
-        )
+        comment_body += f"[Download the compiled PDF](https://github.com/{REPO}/actions/artifacts/{RUN_ID}?artifactName={ARTIFACT_NAME})\n"
 
     with open("comment_body.md", "w") as file:
         file.write(comment_body)
